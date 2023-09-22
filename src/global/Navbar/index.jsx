737fc7a22065949f2e03c3a144fc5e229d9a9b9b
theAvocadoCoder <nwauwa.kelechi.e@gmail.com>
2023-09-22 17:37:44 +0100

    fix: adjust nav icons on mobile

--- src/global/Navbar/index.jsx
+++ src/global/Navbar/index.jsx
@@ -23,10 +23,10 @@ function Navbar() {
         <img src={logo} alt="" className={`${isSidebarOpen ? "hidden lg:block" : ""} h-[.937rem] lg:h-[2.25rem]`} />
       </Link>
 
-      <button type="button" className={`w-6 h-6 bg-[url('assets/burger.svg')] bg-contain bg-no-repeat flex justify-center items-center text-center p-0 lg:hidden`} aria-label="Open Menu" onClick={toggleSideBar} />
+      <button type="button" className={`w-[1.1875rem] h-[.875rem] bg-[url('assets/burger.svg')] bg-contain bg-no-repeat flex justify-center items-center text-center p-0 lg:hidden`} aria-label="Open Menu" onClick={toggleSideBar} />
       
       <div className={`fixed flex flex-col justify-center gap-10 w-screen h-screen px-16 md:px-32 md:text-[1.5rem] top-0 left-0 font-medium lg:text-[1.125rem] 2xl:text-[1.75rem] lg:w-[min(68%,40rem)] xl:w-[min(65%,58rem)] lg:h-fit lg:flex-row lg:gap-0 lg:justify-end lg:static lg:p-0 ${isSidebarOpen ? "flex bg-indigo-gl lg:bg-transparent" : "hidden lg:flex"}`}>
-        <button type="button" className={`w-6 h-6 bg-[url('assets/exit.svg')] bg-contain bg-no-repeat flex justify-center items-center text-center p-0 lg:hidden absolute top-16 right-16`} aria-label="Close Menu" onClick={toggleSideBar} />
+        <button type="button" className={`w-[1.4375rem] md:w-8 md:h-8 h-[1.4375rem] bg-[url('assets/exit.svg')] bg-contain bg-no-repeat flex justify-center items-center text-center p-0 lg:hidden absolute top-[2.4375rem] right-[2.4375rem] md:top-14 md:right-14`} aria-label="Close Menu" onClick={toggleSideBar} />
 
         <ul className="flex flex-col lg:flex-row gap-10 items-start lg:items-center w-fit">
           {["Timeline", "Overview", "FAQs", "Contact"].map((item, idx) => {
