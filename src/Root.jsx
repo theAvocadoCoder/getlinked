6cae09c57ac6e13b988c8c49aac9e83432e62337
theAvocadoCoder <nwauwa.kelechi.e@gmail.com>
2023-09-23 21:38:50 +0100

    fix: cosmetic change to Navbar and Root

--- src/Root.jsx
+++ src/Root.jsx
@@ -1,17 +1,13 @@
-import { Outlet, useLocation } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import Navbar from "global/Navbar";
-import Footer from "global/Footer";
 
 function Root() {
-  const path = useLocation().pathname;
-
   return (
-    <div className="min-h-screen flex flex-col font-montserrat bg-indigo-gl text-white [&_*]:[-webkit-tap-highlight-color:transparent]">
+    <div className="h-screen flex flex-col font-montserrat bg-indigo-gl text-white [&_*]:[-webkit-tap-highlight-color:transparent]">
       <Navbar />
-      <div className="min-h-[calc(100vh-6.937rem)] sm:min-h-[calc(100vh-8.25rem)] h-full flex items-center justify-center overscroll-y-scroll">
+      <div className="h-full w-full overflow-y-scroll">
         <Outlet />
       </div>
-      {path == "/" && (<Footer />)}
     </div>
   )
 }
